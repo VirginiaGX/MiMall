f7ac1b335bdff0ef1794f7dad094f0bb7e26dc20
gx <gx13710626075@163.com>
2020-03-14 19:02:14 +0800

    接口错误拦截

--- src/main.js
+++ src/main.js
@@ -1,7 +1,24 @@
 import Vue from 'vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import App from './App.vue'
 
+// 根据前端跨域方式作调整  /a/b: /api/a/b=> /a/b
+axios.defaults.baseURL = '/api'
+axios.defaults.timeout = 8000
+// 接口错误拦截
+axios.interceptors.response.use(function(response){
+  let res = response.data
+  if(res.status === 0){
+    return res.data
+  } else if(res.status === 10){
+    window.location.href = '/#/login'
+  } else {
+    alert(res.msg)
+  }
+})
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
 new Vue({
