22d1d605a0799ef16b32409d6b1664614ba01292
gx <gx13710626075@163.com>
2020-03-21 14:40:01 +0800

    footer组件开发

--- src/components/NavFooter.vue
+++ src/components/NavFooter.vue
@@ -1,5 +1,17 @@
 <template>
-<div></div>
+<div class="footer">
+  <div class="footer-logo">
+    <img src="/imgs/logo-footer.png" alt=""/>
+    <p>小米商城</p>
+  </div>
+  <div class="footer-link">
+    <a href="https://coding.imooc.com/learn/list/397.html" target="_blank">河畔一脚主页</a><span>|</span>
+    <a href="" target="_blank">vue全站课程</a><span>|</span>
+    <a href="" target="_blank">react全家</a><span>|</span>
+    <a href="" target="_blank">微信支付专项</a>
+  </div>
+  <div class="copyright">哈哈<span class="domain">jjjjmmmmmm</span>哈哈哈</div>
+</div>
 </template>
 
 <script>
@@ -7,3 +19,36 @@ export default {
   name: 'nav-footer'
 }
 </script>
+<style lang="scss">
+.footer{
+  height: 234px;
+  border-top: 4px solid #FF6600;
+  background-color: #333333;
+  color: #999999;
+  font-size: 16px;
+  text-align: center;
+  .footer-logo{
+    margin-top:46px;
+    margin-bottom: 31px;
+    img{
+      width: 53px;
+      margin-bottom: 13px;
+    }
+  }
+  .footer-link{
+    a{
+      color: #999999;
+      display: inline-block;
+    }
+    span{
+      margin: 0 10px;
+    }
+    margin-bottom: 13px;
+  }
+  .copyright{
+    .domain{
+      color: #FF6600;
+    }
+  }
+}
+</style>
---
+++ src/components/ServiceBar.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  <div class="service"></div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'service-bar',
+  components: {
+  }
+}
+</script>
+<style lang="scss">
+</style>
