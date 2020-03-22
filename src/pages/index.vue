d82570798022c877693be03d9b97121008b91562
gx <gx13710626075@163.com>
2020-03-22 15:31:11 +0800

    手机商品实现二

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -70,14 +70,14 @@
           <div class="list-box">
             <div class="list" v-for="(arr,i) in phoneList" v-bind:key="i">
               <div class="item" v-for="(item,j) in arr" v-bind:key="j">
-                <span>新品</span>
+                <span v-bind:class="{'new-pro':j%2==0}">新品</span>
                 <div class="item-img">
-                  <img src="https://cdn.cnbj1.fds.api.mi-img.com/mi-mall/6f2493e6c6fe8e2485c407e5d75e3651.jpg" alt="">
+                  <img v-bind:src="item.mainImage" alt="">
                 </div>
                 <div class="item-info">
-                  <h3>小米9</h3>
-                  <p>骁龙855，索尼4800万超广角微距</p>
-                  <p class="price">2999元</p>
+                  <h3>{{item.name}}</h3>
+                  <p>{{item.subtitle}}</p>
+                  <p class="price">{{item.price}}元</p>
                 </div>
               </div>
             </div>
@@ -183,9 +183,24 @@ export default {
             img:'/imgs/ads/ads-4.jpg'
           }
         ],
-        phoneList:[[1,1,1,1,],[1,1,1,1]]
+        phoneList:[]
     }
   },
+  mounted(){
+      this.init();
+    },
+  methods: {
+     init(){
+        this.axios.get('/products',{
+          params:{
+            categoryId:100012,
+            pageSize:8
+          }
+        }).then((res)=>{
+          this.phoneList = [res.list.slice(0,4),res.list.slice(4,8)];
+        })
+      }
+  }
 }
 </script>
 <style lang="scss">
@@ -316,11 +331,23 @@ export default {
               height:302px;
               background-color:$colorG;
               text-align:center;
-/*               span{
-
-              } */
+               span{
+                display:inline-block;
+                width:67px;
+                height:24px;
+                font-size:14px;
+                line-height:24px;
+                color:$colorG;
+                &.new-pro{
+                  background-color:#7ECF68;
+                }
+                &.kill-pro{
+                  background-color:#E82626;
+                }
+              } 
               .item-img{
                 img{
+                  width:100%;
                   height:195px;
                 }
               }
